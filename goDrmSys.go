eeff2ef38737b82f9700526e69c2d090def176c7
tiwariHD <tiwari@stud.uni-heidelberg.de>
2017-05-16 16:04:22 +0200

    version 0.0.2 first commit

--- goDrmSys.go
+++ goDrmSys.go
@@ -11,172 +11,212 @@ int getNumDevices(void)
 */
 import "C"
 import (
-    "unsafe"
-    "fmt"
+	"fmt"
+	"unsafe"
 )
 
+//constants mapping constants from libdrm
 const (
-    DRM_BUS_PCI         = 0
-    DRM_BUS_USB         = 1
-    DRM_BUS_PLATFORM    = 2
-    DRM_BUS_HOST1X      = 3
+	DRM_BUS_PCI      = 0
+	DRM_BUS_USB      = 1
+	DRM_BUS_PLATFORM = 2
+	DRM_BUS_HOST1X   = 3
 )
 
+//DrmPciBusInfo maps to drmPciBusInfo of libdrm
 type DrmPciBusInfo struct {
-    Domain      uint16
-    Bus         uint8
-    Dev         uint8
-    Func        uint8
+	Domain uint16
+	Bus    uint8
+	Dev    uint8
+	Func   uint8
 }
 
+//DrmPciDeviceInfo maps to drmPciDeviceInfo of libdrm
 type DrmPciDeviceInfo struct {
-    VendorId    uint16
-    DeviceId    uint16
-    SubVendorid uint16
-    SubDeviceId uint16
-    RevisionId  uint8
+	VendorID    uint16
+	DeviceID    uint16
+	SubVendorID uint16
+	SubDeviceID uint16
+	RevisionID  uint8
 }
 
+//DrmUsbBusInfo maps to drmUsbBusInfo of libdrm
 type DrmUsbBusInfo struct {
-    Bus         uint8
-    Dev         uint8
+	Bus uint8
+	Dev uint8
 }
 
+//DrmUsbDeviceInfo maps to drmUsbDeviceInfo of libdrm
 type DrmUsbDeviceInfo struct {
-    Vendor      uint16
-    Product     uint16
+	Vendor  uint16
+	Product uint16
 }
 
+//DrmPlatformBusInfo maps to drmPlatformBusInfo of libdrm
 type DrmPlatformBusInfo struct {
-    FullName    string
+	FullName string
 }
 
+//DrmPlatformDeviceInfo maps to drmPlatformDeviceInfo of libdrm
 type DrmPlatformDeviceInfo struct {
-    Compatible  **C.char    //TODO: conversion
+	Compatible **C.char //TODO: conversion
 }
 
+//DrmHost1xBusInfo maps to drmHost1xBusInfo of libdrm
 type DrmHost1xBusInfo struct {
-    FullName    string
+	FullName string
 }
 
+//DrmHost1xDeviceInfo maps to drmHost1xDeviceInfo of libdrm
 type DrmHost1xDeviceInfo struct {
-    Compatible  **C.char    //TODO: conversion
+	Compatible **C.char //TODO: conversion
 }
 
+//DeviceNodes contains strings for Primary, Render, Contol part of GPU
 type DeviceNodes struct {
-    Primary     string
-    Control     string
-    Render      string
+	NodeMap map[string]string
 }
 
+//BusIn defines the interface for all types of BusInfo
 type BusIn interface {
-    GetBusInfo() string
+	GetBusInfo() string
 }
 
+//GetBusInfo for PciInfo
 func (b DrmPciBusInfo) GetBusInfo() string {
-    return (fmt.Sprintf("%d:%d:%d.%d", b.Domain, b.Bus, b.Dev, b.Func))
+	return (fmt.Sprintf("%d:%d:%d.%d", b.Domain, b.Bus, b.Dev, b.Func))
 }
 
+//GetBusInfo for UsbInfo
 func (b DrmUsbBusInfo) GetBusInfo() string {
-    return (fmt.Sprintf("%d:%d", b.Bus, b.Dev))
+	return (fmt.Sprintf("%d:%d", b.Bus, b.Dev))
 }
 
+//GetBusInfo for PlatformInfo
 func (b DrmPlatformBusInfo) GetBusInfo() string {
-    return (fmt.Sprintf("%s", b.FullName))
+	return (fmt.Sprintf("%s", b.FullName))
 }
 
+//GetBusInfo for Host1xInfo
 func (b DrmHost1xBusInfo) GetBusInfo() string {
-    return (fmt.Sprintf("%s", b.FullName))
+	return (fmt.Sprintf("%s", b.FullName))
 }
 
+//DevIn defines the interface for all types of DeviceInfo
 type DevIn interface {
-    GetDevInfo()
+	GetDevInfo() string
 }
 
-func (b DrmPciDeviceInfo) GetDevInfo() {
-    //add
+//GetDevInfo for PciInfo
+func (b DrmPciDeviceInfo) GetDevInfo() string {
+	return (fmt.Sprintf("0x%X:0x%X:0x%X:0x%X:%d", b.VendorID, b.DeviceID,
+		b.SubVendorID, b.SubDeviceID, b.RevisionID))
 }
 
-func (b DrmUsbDeviceInfo) GetDevInfo() {
-    //add
+//GetDevInfo for UsbInfo
+func (b DrmUsbDeviceInfo) GetDevInfo() string {
+	return (fmt.Sprintf("0x%X:0x%X", b.Vendor, b.Product))
 }
 
-func (b DrmPlatformDeviceInfo) GetDevInfo() {
-    //add
+//GetDevInfo for PlatformInfo
+func (b DrmPlatformDeviceInfo) GetDevInfo() string {
+	//add
+	/*list := []string{}
+	clist := (*[1 << 30]*C.char)(unsafe.Pointer(b.Compatible))[:C.DRM_NODE_MAX]
+	for i := 0; i < len(clist); i++ {
+		list = append(list, C.GoString(clist[i]))
+	}
+	ret := strings.Join(list, ":")*/
+	return ""
 }
 
-func (b DrmHost1xDeviceInfo) GetDevInfo() {
-    //add
+//GetDevInfo for Host1xInfo
+func (b DrmHost1xDeviceInfo) GetDevInfo() string {
+	//add
+	/*list := []string{}
+	clist := (*[1 << 30]*C.char)(unsafe.Pointer(b.Compatible))[:C.DRM_NODE_MAX]
+	for i := 0; i < len(clist); i++ {
+		list = append(list, C.GoString(clist[i]))
+	}
+	ret := strings.Join(list, ":")*/
+	return ""
 }
 
+//DeviceInfo contains BusInfo and Device info fields of drmDevice
 type DeviceInfo struct {
-    BusInfo     BusIn
-    DevInfo     DevIn
+	BusInfo BusIn
+	DevInfo DevIn
 }
 
+//Device contains all info from libdrm struct drmDevice
 type Device struct {
-    Nodes   DeviceNodes
-    Info    DeviceInfo
+	Nodes DeviceNodes
+	Info  DeviceInfo
 }
 
-func (nodes *DeviceNodes) from_sys (d C.drmDevicePtr) {
-    nds := (*[1<<30]*C.char)(unsafe.Pointer(d.nodes))[:C.DRM_NODE_MAX]
+func (nodes *DeviceNodes) fromSys(d C.drmDevicePtr) {
+	nodes.NodeMap = make(map[string]string)
+	nds := (*[1 << 30]*C.char)(unsafe.Pointer(d.nodes))[:C.DRM_NODE_MAX]
 
-    if (d.available_nodes & (1 << C.DRM_NODE_PRIMARY)) != 0 {
-        nodes.Primary = C.GoString(nds[C.DRM_NODE_PRIMARY])
-    }
+	if (d.available_nodes & (1 << C.DRM_NODE_PRIMARY)) != 0 {
+		nodes.NodeMap["primary"] = C.GoString(nds[C.DRM_NODE_PRIMARY])
+		//nodes.Primary = C.GoString(nds[C.DRM_NODE_PRIMARY])
+	}
 
-    if (d.available_nodes & (1 << C.DRM_NODE_CONTROL)) != 0 {
-        nodes.Control = C.GoString(nds[C.DRM_NODE_CONTROL])
-    }
+	if (d.available_nodes & (1 << C.DRM_NODE_CONTROL)) != 0 {
+		nodes.NodeMap["control"] = C.GoString(nds[C.DRM_NODE_CONTROL])
+		//nodes.Control = C.GoString(nds[C.DRM_NODE_CONTROL])
+	}
 
-    if (d.available_nodes & (1 << C.DRM_NODE_RENDER)) != 0 {
-        nodes.Render = C.GoString(nds[C.DRM_NODE_RENDER])
-    }
+	if (d.available_nodes & (1 << C.DRM_NODE_RENDER)) != 0 {
+		nodes.NodeMap["render"] = C.GoString(nds[C.DRM_NODE_RENDER])
+		//nodes.Render = C.GoString(nds[C.DRM_NODE_RENDER])
+	}
 
 }
 
-func (info *DeviceInfo) from_sys (d C.drmDevicePtr) {
-    switch int(d.bustype) {
+func (info *DeviceInfo) fromSys(d C.drmDevicePtr) {
+	switch int(d.bustype) {
 
-    case DRM_BUS_PCI :
-        info.BusInfo = *(*(*DrmPciBusInfo))(unsafe.Pointer(&d.businfo))
-        info.DevInfo = *(*(*DrmPciDeviceInfo))(unsafe.Pointer(&d.deviceinfo))
+	case DRM_BUS_PCI:
+		info.BusInfo = *(*(*DrmPciBusInfo))(unsafe.Pointer(&d.businfo))
+		info.DevInfo = *(*(*DrmPciDeviceInfo))(unsafe.Pointer(&d.deviceinfo))
 
-    case DRM_BUS_USB :
-        info.BusInfo = *(*(*DrmUsbBusInfo))(unsafe.Pointer(&d.businfo))
-        info.DevInfo = *(*(*DrmUsbDeviceInfo))(unsafe.Pointer(&d.deviceinfo))
+	case DRM_BUS_USB:
+		info.BusInfo = *(*(*DrmUsbBusInfo))(unsafe.Pointer(&d.businfo))
+		info.DevInfo = *(*(*DrmUsbDeviceInfo))(unsafe.Pointer(&d.deviceinfo))
 
-    case DRM_BUS_PLATFORM :
-        info.BusInfo = *(*(*DrmPlatformBusInfo))(unsafe.Pointer(&d.businfo))
-        info.DevInfo = *(*(*DrmPlatformDeviceInfo))(unsafe.Pointer(&d.deviceinfo))
+	case DRM_BUS_PLATFORM:
+		info.BusInfo = *(*(*DrmPlatformBusInfo))(unsafe.Pointer(&d.businfo))
+		info.DevInfo = *(*(*DrmPlatformDeviceInfo))(unsafe.Pointer(&d.deviceinfo))
 
-    case DRM_BUS_HOST1X :
-        info.BusInfo = *(*(*DrmHost1xBusInfo))(unsafe.Pointer(&d.businfo))
-        info.DevInfo = *(*(*DrmHost1xDeviceInfo))(unsafe.Pointer(&d.deviceinfo))
-    }
+	case DRM_BUS_HOST1X:
+		info.BusInfo = *(*(*DrmHost1xBusInfo))(unsafe.Pointer(&d.businfo))
+		info.DevInfo = *(*(*DrmHost1xDeviceInfo))(unsafe.Pointer(&d.deviceinfo))
+	}
 }
 
+//DrmAvailable checks whether DRM is available
 func DrmAvailable() bool {
 
-    return (C.drmAvailable() != 0)
+	return (C.drmAvailable() != 0)
 }
 
+//GetDevices returns array containing info for all devices
 func GetDevices() []Device {
 
-    n := C.getNumDevices()
-    d := make([]C.drmDevicePtr, n)
-    C.drmGetDevices(&d[0], n)
+	n := C.getNumDevices()
+	d := make([]C.drmDevicePtr, n)
+	C.drmGetDevices(&d[0], n)
 
-    devices := make([]Device, n)
-    for i := 0; i < int(n); i++ {
-        devices[i].Nodes.from_sys(d[i])
-        devices[i].Info.from_sys(d[i])
-    }
+	devices := make([]Device, n)
+	for i := 0; i < int(n); i++ {
+		devices[i].Nodes.fromSys(d[i])
+		devices[i].Info.fromSys(d[i])
+	}
 
-    C.drmFreeDevices(&d[0], n)
+	C.drmFreeDevices(&d[0], n)
 
-    return devices
+	return devices
 
 }
-
